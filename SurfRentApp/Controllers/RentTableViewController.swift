08b5844273a25ca598e156592131c3a6fff709c7
down ultra <67225488+ultradown@users.noreply.github.com>
2021-07-30 20:54:39 +0300

    fixed mistakes

--- SurfRentApp/Controllers/RentTableViewController.swift
+++ SurfRentApp/Controllers/RentTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class RentTableViewController: UITableViewController {
 
+    let images = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,20 +20,22 @@ class RentTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return images.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RentTableViewCell
 
-        // Configure the cell...
+        cell.myName.text = images[indexPath.row]
 
+        cell.myImage.image = UIImage(named: images[indexPath.row])
+        
         return cell
     }
 }
---
+++ SurfRentApp/Models/RentTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RentTableViewCell.swift
+//  SurfRentApp
+//
+//  Created by Daniil Belyaev on 30.07.2021.
+//
+
+import UIKit
+
+class RentTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myName: UILabel!
+    
+    @IBOutlet weak var myStatus: UILabel!
+}
